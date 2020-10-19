0dcf6f79210838325929828c134a0e756793033d
StevenGrootaert <steve.grootaert@gmail.com>
2020-10-19 01:47:17 -0400

    Creating the Note Service

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -17,6 +17,8 @@ namespace ElevenNote.Data
 
         [Required]
         public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
 
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
